c1e1f4f7f6569d1742c2cb15c85e7883fe7dafd6
Ivan Silva <silvaivan280@gmail.com>
2024-12-17 08:07:17 -0300

    agregue el color del cursor

--- src/components/common/Portada.jsx
+++ src/components/common/Portada.jsx
@@ -4,31 +4,38 @@ import github from "../../../public/github.png";
 import SobreMi from "./SobreMi";
 import Tecnologias from "./Tecnologias";
 
-
 const Portada = () => {
-    return (
-        <Container className="text-center">
-            <section className="contenedor-titulo">
-                <div className="maquina-escribir">
-                <h1 className="titulo">HI, I'M IVÁN SILVA 👋</h1>
-                </div>
-                <h3 className="texto-secundario">Fullstack Developer</h3>
-            </section>
-            <section className="mt-5">
-                <Button className="btn btn-resume texto-secundario">Download Resume</Button>
-            </section>
-            <section className="mt-5">
-                <a href="https://www.linkedin.com/in/ivan-silva22/" target="_blanck">
-                    <img className="logo-contacto me-4" src={linkedin} alt="Logo de linkedin" />
-                </a>
-                <a href="https://github.com/ivan-silva22" target="_blanck">
-                    <img className="logo-contacto" src={github} alt="Logo de github" />
-                </a>
-            </section>
-            <SobreMi/>
-            <Tecnologias/>
-        </Container>
-    );
+  return (
+    <main className="contenedor-principal">
+      <Container className="text-center portada">
+        <section className="contenedor-titulo">
+          <div className="maquina-escribir">
+            <h1 className="titulo">HI, I'M IVÁN SILVA 👋</h1>
+          </div>
+          <h3 className="texto-secundario">Fullstack Developer</h3>
+        </section>
+        <section className="mt-5">
+          <Button className="btn btn-resume texto-secundario">
+            Download Resume
+          </Button>
+        </section>
+        <section className="mt-5">
+          <a href="https://www.linkedin.com/in/ivan-silva22/" target="_blanck">
+            <img
+              className="logo-contacto me-4"
+              src={linkedin}
+              alt="Logo de linkedin"
+            />
+          </a>
+          <a href="https://github.com/ivan-silva22" target="_blanck">
+            <img className="logo-contacto" src={github} alt="Logo de github" />
+          </a>
+        </section>
+        <SobreMi />
+        <Tecnologias />
+      </Container>
+    </main>
+  );
 };
 
 export default Portada;
